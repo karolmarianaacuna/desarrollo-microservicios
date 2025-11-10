31e8f5916eccedc6806359a44d9ad13641fc9504
Mariana <karolmarianaacuna@gmail.com>
2025-11-10 08:08:52 -0500

    Actualizo microservicio de frontend redireccion - inicio de sesion

--- microservicio-frontend/js/login.js
+++ microservicio-frontend/js/login.js
@@ -1,18 +1,16 @@
-const formualrio = document.getElementById('loginForm');
-formualrio.addEventListener('submit', async(e)=>{
+const fomulario = document.getElementById('loginForm');
+fomulario.addEventListener('submit', async(e)=>{
     e.preventDefault(); //Evita que la pagina se recargue
     
     //Es como para que los datos no se vayan sin antes haber sido capturados 
 
     //caputaramos los datos que el usuario manda por la parte del frontend 
-    const email = document.getElementById('email');
-    const password = document.getElementById('password');
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
     //como hace una conexion o un envio que puede falla por eso usamos el try/catch 
     try {
-        
         //Eviamos datos al backend 
-
          const response = await axios.post('http://localhost:4000/api/auth/login', {
       email,
       password
